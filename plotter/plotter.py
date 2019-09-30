2beff1b65b09b1acc245739d16987a0a86fbb0d9
avigh <avightclav@gmail.com>
2019-09-30 11:53:47 +0300

    FIX: Plotter set title and labels from args

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -8,5 +8,8 @@ class Plotter:
     def plot(signal: signals.Signal, _title ="Signal", x_label ="time", y_label ="V"):
         fig = figure()
         splt = fig.add_subplot(111)
+        splt.set_title(_title)
+        splt.set_xlabel(x_label)
+        splt.set_ylabel(y_label)
         splt.plot(signal.get_x(), signal.get_y())
         fig.show()
