0ba678253cb73a16b5bb1b5f3577deabf8dc978e
DanilaEremenko <danilaeremenko@mail.ru>
2019-11-20 22:12:14 +0300

    SignalProcessing: auto title formattion added

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -6,9 +6,18 @@ from signals import signals
 
 class Plotter:
     @staticmethod
-    def plot(signal: signals.Signal, t_start=0, t_end=None, _title="Signal", x_label="time", y_label="V"):
+    def plot(signal: signals.Signal, t_start=0, t_end=None, _title=None, x_label="time", y_label="V"):
         fig = figure(dpi=200)
         splt = fig.add_subplot(111)
+
+        if _title is None:
+            if isinstance(signal, signals.DoubleSideband):
+                _title = "Double Sideband"
+            elif isinstance(signal, signals.Detect):
+                _title = "Dected Signal"
+            else:
+                _title = "Signal"
+
         splt.set_title(_title)
         splt.set_xlabel(x_label)
         splt.set_ylabel(y_label)
@@ -33,4 +42,5 @@ class Plotter:
         print(f_fft[last_nonzero_index])
         splt.set_xlim(-f_fft[last_nonzero_index] - spacing, f_fft[last_nonzero_index] + spacing)
         splt.plot(f_fft, signal_fft)
+        splt.set_title("Fourier spectrum")
         fig.show()
